427930c1ec94ab665987f11156b2126f219b12c3
Vojislav Kovanusic <mladisumar@yahoo.com>
2017-02-19 16:10:21 +0100

    dodatak

---
+++ src/zadaci_13_02_2017Bonus/BonusZadatakBr3.java
@@ -0,0 +1,36 @@
+package zadaci_13_02_2017Bonus;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class BonusZadatakBr3 {
+
+	public static void main(String[] args) {
+		Scanner unos = new Scanner(System.in);
+		double brSvjGod = 0;
+		try {
+			System.out.print("Unesite broj svjetlosnih godina: ");
+			brSvjGod = unos.nextDouble();
+			distance(brSvjGod);
+		} catch (Exception e) {
+			System.out.println("Pogresan unos. ");
+		}
+		unos.close();
+	}
+
+	public static void distance(double svjetlosniG) {
+		DecimalFormat format = new DecimalFormat("##.##");
+		long sec = 365 * 24 * 60 * 60;
+		long ly = 299792458 * sec;
+		long totalKilometers = (long) ((ly * svjetlosniG) / 1000);
+		double astro_jedinica = (totalKilometers) / 149600000 * svjetlosniG;
+		double parsec = 1 / 3.26 * svjetlosniG;
+		double helios = totalKilometers / 252792.537;
+
+		System.out.println(svjetlosniG + " svjetlosnih godina je: \n - "
+				+ totalKilometers + " kilometara\n - " + astro_jedinica
+				+ " astronomskih jedinica\n - " + parsec
+				+ " parseka\n Helios 2 bi presao za: " + format.format(helios)
+				+ " h.");
+	}
+}
