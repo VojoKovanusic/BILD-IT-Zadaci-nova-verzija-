397106610088b5a30b64ed54ef52d59a9f5fd3e6
Vojislav Kovanusic <mladisumar@yahoo.com>
2017-03-10 00:13:35 +0100

    Korekcije ponovni unos slova, ispis trazene rjeci nakon poraza.

--- src/Mini4/Hangman.java
+++ src/Mini4/Hangman.java
@@ -1,5 +1,6 @@
 package Mini4;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hangman {
@@ -7,15 +8,17 @@ public class Hangman {
 	static Scanner unos = new Scanner(System.in);
 
 	static String[] nizRjeci = { "heap", "array", "double", "short", "math",
-			"main", "this", "public","program","Inheritance", "polymorphism", "void","class" };
-
+			"main", "this", "public", "program", "Inheritance", "polymorphism",
+			"void", "class" };
+	// ovdje ce se smestati dupli unosi
+	static ArrayList<Character> temp = new ArrayList<>();
 	// program sam bira nasumice index u nizu
 	static int index = (int) (Math.random() * nizRjeci.length);
 	// izabranu rjec pretvorio u niz karaktera, da mogu lakse baratati
 	static char slova[] = nizRjeci[index].toCharArray();
 	// niz u koji ce mo smjestati pogodjena slova,
 	static char[] deponujUnose = new char[slova.length];
-	 
+
 	static boolean uslov = false;
 
 	public static void main(String[] args) {
@@ -56,14 +59,22 @@ public class Hangman {
 		return uslov;
 	}
 
-	// ova metoda sluzi za brpojanje promasenih slova(vraca 1), ali ujedno i
+	// ova metoda sluzi za brojanje promasenih slova(vraca 1), ali ujedno i
 	// ispisuje epilog posle unosa slova
 	public static int odgovor(char c) {
 		// prolazimo kroz sva slova
 		for (int i = 0; i < slova.length; i++) {
 			// ako se poklapa prosledjeni argument sa karakterom u nizu pamtmo
 			// ga
+			if (ponovljenoSlovo(c)) {
+				System.out.println("Vec ste jednom unjeli slovo " + c
+						+ ", sada ste korak blize vjesnju");
+				return 1;
+			}
+
 			if (c == slova[i]) {
+				// pamtimo unesena slova
+				temp.add(c);
 				deponujUnose[i] = c;
 				System.out.println("Slovo \"" + c + "\" se nalazi ispod ?");
 				return 0;
@@ -71,9 +82,21 @@ public class Hangman {
 
 		}
 		System.out.println("Slovo \"" + c
-				+ "\" se ne nalazi ispod ?, idete na vjesala!");
+				+ "\" se ne nalazi ispod ? , jedan korak ste blize vjesanju!");
 		return 1;
 
+	}
+
+	// metoda vraca true u koliko je karakter u listi
+
+	private static boolean ponovljenoSlovo(char c) {
+		for (int i = 0; i < temp.size(); i++) {
+			if (temp.get(i) == c) {
+				return true;
+			}
+
+		}
+		return false;
 	}
 
 	// metoda koja printa trenutno stanje sa slovim-upitnicima
@@ -115,6 +138,7 @@ public class Hangman {
 			System.out.println("\n __\n|  |\n|  o\n|  /\\\n/\\");
 			System.out
 					.println("Upravo ste objeseni, kraj igre!!!\nVise srece drugi put.");
+			System.out.println("Trazena rjec je bila: \""+nizRjeci[index]+"\"!");
 			uslov = true;
 			break;
 		case 10:
